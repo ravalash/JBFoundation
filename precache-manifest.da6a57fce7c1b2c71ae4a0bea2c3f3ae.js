83566750ca48a448de21d56155b1f96d767f2352
ravalash <13k0873juhky@opayq.net>
2020-10-07 19:49:43 -0400

    Updates

---
+++ precache-manifest.da6a57fce7c1b2c71ae4a0bea2c3f3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79348e91d5ad94614e8116a3ea0a8f7",
+    "url": "/JBFoundation/index.html"
+  },
+  {
+    "revision": "38f2de2dbcafb5e8fed6",
+    "url": "/JBFoundation/static/css/main.c072e582.chunk.css"
+  },
+  {
+    "revision": "f6fb8827fb911f3903db",
+    "url": "/JBFoundation/static/js/2.1fbeb9d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JBFoundation/static/js/2.1fbeb9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f2de2dbcafb5e8fed6",
+    "url": "/JBFoundation/static/js/main.bc9677b8.chunk.js"
+  },
+  {
+    "revision": "abc323c83136e18eccbb",
+    "url": "/JBFoundation/static/js/runtime-main.c6eb0892.js"
+  }
+]);
